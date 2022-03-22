aa4ec992de50133e91d225ce83a9f3f9adaaf6a1
bauerDOTuzh <adam.bauer@uzh.ch>
2022-03-22 18:03:52 +0100

    group name added

--- src/components/views/Logo.js
+++ src/components/views/Logo.js
@@ -7,7 +7,7 @@ function Logo() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Group 23
                 </p>
                 <a
                     className="App-link"
