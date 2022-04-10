78deae6fcb2b7697b7a6a0f3cedf930e4846dbe9
dueringm <mark.duering@uzh.ch>
2022-04-10 16:35:33 +0200

    Functioning Login/Register closes #1, closes #4

--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -1,15 +1,34 @@
 import React from "react";
 import EventItem from "../ui/EventItem";
 import { SearchBar } from "../ui/SearchBar";
+import { useNavigate } from "react-router";
 import FilterButton from "../ui/FilterButton";
-import { VerticalLineDashboard } from "../ui/VerticalLineDashboard";
 import ProfileOverview from "../ui/ProfileOverview";
 import DashboardButtons from "../ui/DashboardButtons";
 import EventItemSquare from "../ui/EventItemSquare";
 import "../../styles/_theme.scss";
 import Header from "./Header";
+import {MyButton} from "../ui/MyButton";
+import {apiLoggedIn, handleError} from "../../helpers/api";
 
 const Dashboard = (props) => {
+
+  const navigate = useNavigate();
+  let userId = localStorage.getItem('userId');
+
+  const logout = async () => {
+    try {
+      await apiLoggedIn().put(`/logout/${userId}`);
+      // Remove the token from the local storage.
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
+      navigate('/');
+
+    } catch (error) {
+      alert(`Something went wrong during logout: \n${handleError(error)}`);
+    }
+  };
+
   return (
     <div>
       <Header />
@@ -42,6 +61,7 @@ const Dashboard = (props) => {
           </div>
         </div>
       </div>
+      <MyButton onClick={() => logout()}>Logout</MyButton>
     </div>
   );
 };
--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -4,58 +4,46 @@ import { useState } from "react";
 import "./../../styles/views/Login.scss";
 import { MyButton } from "../ui/MyButton";
 import { useNavigate } from "react-router";
+import {api, handleError} from '../../helpers/api';
 
 const Login = (props) => {
-  const [password, setPassword] = useState(null);
   const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
   const navigate = useNavigate();
 
   const doLogin = async () => {
-    console.log("Loggin in!");
-    navigate("/home");
-  };
-  const doRegister = async () => {
-    console.log("Registering!");
+    try {
+      const requestBody = JSON.stringify({username, password});
+      const response = await api.post('/login', requestBody);
+      //response is user returned from server
+
+      // Store the token into the local storage.
+      localStorage.setItem('token', response.headers.token);
+      localStorage.setItem('userId', response.data.id);
+      navigate('/home')
+
+    } catch (error) {
+      alert(`Something went wrong during the login: \n${handleError(error)}`);
+    }
   };
 
   return (
-    <div className={"BackgroundImage align-content-center min-vh-100 pt-5"}>
-      <div className={"LoginField col-4 offset-4"}>
-        <div className={"bg-light rounded-3 my-3 py-5 mx-3"}>
-          <div className={"row justify-content-center"}>
-            <div className={"col-12 text-center login-text py-1 mb-3"}>
-              Login to access your Account.
-            </div>
-            <div className={"col-12 text-center py-1"}>
-              <FormField onChange={(un) => setUsername(un)} />
-            </div>
-            <div className={"col-12 text-center py-1"}>
-              <PasswordField onChange={(pw) => setPassword(pw)} />
-            </div>
-            <div className={"col-12 text-center pt-1 pb-3 mt-3"}>
-              <MyButton onClick={() => doLogin()}>Login</MyButton>
-            </div>
-          </div>
-
-          <hr className={"mx-5"} />
-
-          <div className={"row"}>
-            <div className={"col-12 text-center login-text pt-3 pb-1 mb-3"}>
-              Register with a few steps.
-            </div>
-            <div className={"col-12 text-center py-1"}>
-              <FormField onChange={(un) => setUsername(un)} />
-            </div>
-            <div className={"col-12 text-center py-1"}>
-              <PasswordField onChange={(pw) => setPassword(pw)} />
-            </div>
-            <div className={"col-12 text-center py-1 mt-3"}>
-              <MyButton onClick={() => doRegister()}>Register</MyButton>
-            </div>
-          </div>
+      <div className={"marginClass mx-auto w-25"}>
+        <div className={"col-12 text-center login-text py-1 mb-3"}>
+          Login to access your Account.
         </div>
+        <div className={"col-12 text-center py-1"}>
+          <FormField onChange={(un) => setUsername(un)} />
+        </div>
+        <div className={"col-12 text-center py-1"}>
+          <PasswordField onChange={(pw) => setPassword(pw)} />
+        </div>
+        <div className={"col-12 text-center pt-1 pb-3 mt-3"}>
+          <MyButton onClick={() => doLogin()}>Login</MyButton>
+        </div>
+        <hr className={"mx-5"} />
       </div>
-    </div>
   );
 };
 
