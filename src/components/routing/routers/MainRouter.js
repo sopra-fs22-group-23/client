ded339366e06382d3d074822f51b8262bd45835d
dueringm <mark.duering@uzh.ch>
2022-04-11 14:39:48 +0200

    Route Protection added closes #53

--- src/components/routing/routers/MainRouter.js
+++ src/components/routing/routers/MainRouter.js
@@ -5,16 +5,19 @@ import Login from "../../views/Login";
 import User from "../../views/User";
 import Event from "../../views/Event";
 import Register from "../../views/Register";
+import ProtectedRoute from "../protectors/ProtectedRoute";
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<><Login/><Register/></>}/>
-        <Route path="/home" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/event/:id" element={<Event event />} />
+          <Route exact path="/" element={<><Login/><Register/></>}/>
+          <Route element={<ProtectedRoute/>}>
+              <Route path="/home" element={<Dashboard />} />
+              <Route path="/user" element={<User />} />
+          </Route>
+          <Route path="/event/:id" element={<Event event />} />
+          <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
