76a8e57ab550e47c10fc4559779c5c1cde74deb2
paolobss <paolo.tyyko@uzh.ch>
2022-04-13 10:06:33 +0200

    Minor fixes for EventOverview

--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -6,23 +6,21 @@ import EventItemSquare from "../ui/EventItemSquare";
 import "../../styles/_theme.scss";
 import Header from "./Header";
 import "../../styles/views/Dashboard.scss";
-import {MyButton} from "../ui/MyButton";
-import {apiLoggedIn, handleError} from "../../helpers/api";
-import {useNavigate} from "react-router";
+import { MyButton } from "../ui/MyButton";
+import { apiLoggedIn, handleError } from "../../helpers/api";
+import { useNavigate } from "react-router";
 
 const Dashboard = (props) => {
-
   const navigate = useNavigate();
-  let userId = localStorage.getItem('userId');
+  let userId = localStorage.getItem("userId");
 
   const logout = async () => {
     try {
-      await apiLoggedIn().put(`/logout/${userId}`);
+      await apiLoggedIn.put(`/logout/${userId}`);
       // Remove the token from the local storage.
-      localStorage.removeItem('token');
-      localStorage.removeItem('userId');
-      navigate('/');
-
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      navigate("/");
     } catch (error) {
       alert(`Something went wrong during logout: \n${handleError(error)}`);
     }
@@ -51,7 +49,7 @@ const Dashboard = (props) => {
         </div>
       </div>
       <MyButton onClick={() => logout()}>Logout</MyButton>
-      </>
+    </>
   );
 };
 
