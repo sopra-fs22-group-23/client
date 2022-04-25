a18d9c54d16ff800d3dee39d3764638b1ab4c49d
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2022-04-25 15:37:46 +0200

    Update getDomain.js

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -2,7 +2,7 @@ import { isProduction } from "../helpers/isProduction";
 
 export const getDomain = () => {
   
-    const prodUrl = 'https://sopra-fs22-group23-client.herokuapp.com/';
+    const prodUrl = 'https://sopra-fs22-group23-server.herokuapp.com/';
     const devUrl = 'http://localhost:8080';
 
     return isProduction() ? prodUrl : devUrl;
