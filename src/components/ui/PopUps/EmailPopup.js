f8939d900175bc75824466574a149f5568757e98
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2022-05-29 11:24:30 +0200

    Confetti on login and on join public event

--- src/components/ui/PopUps/EmailPopup.js
+++ src/components/ui/PopUps/EmailPopup.js
@@ -1,12 +1,67 @@
-import React, {useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import {apiLoggedIn, handleError} from "../../../helpers/api";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+const canvasStyles = {
+    position: "fixed",
+    pointerEvents: "none",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0
+};
 
 const EmailPopup = (props) => {
     const [email, setEmail] = useState(null);
     const [popupState, setPopupState] = useState("email")
 
+    const refAnimationInstance = useRef(null);
+
+    const getInstance = useCallback((instance) => {
+        refAnimationInstance.current = instance;
+    }, []);
+
+    const makeShot = useCallback((particleRatio, opts) => {
+        refAnimationInstance.current &&
+        refAnimationInstance.current({
+            ...opts,
+            origin: { y: 0.7 },
+            particleCount: Math.floor(200 * particleRatio)
+        });
+    }, []);
+
+    const fire = useCallback(() => {
+        makeShot(0.25, {
+            spread: 26,
+            startVelocity: 55
+        });
+
+        makeShot(0.2, {
+            spread: 60
+        });
+
+        makeShot(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 45
+        });
+    }, [makeShot]);
+
     const sendEmail = async () => {
         try {
+            setPopupState("delivery");
             const requestBody = JSON.stringify({email});
             await apiLoggedIn().post(`/emailNotification/${props.event.id}`, requestBody);
             setPopupState("confirmation");
@@ -28,7 +83,7 @@ const EmailPopup = (props) => {
             />
             <button
                 className={"email-button"}
-                onClick={() => sendEmail()}
+                onClick={() => {sendEmail()}}
             >Send</button>
         </div>
     );
@@ -40,16 +95,28 @@ const EmailPopup = (props) => {
             <div className={"confirmation-content"}>"{props.event.title}"</div>
         </div>
     );
+    const delivery = (
+        <div>
+            <div className={"confirmation-title"}>The email is on the way, please wait.....</div>
+        </div>
+    );
 
     function chooseContent() {
         if(popupState === "confirmation"){
+            fire();
             return confirmationPhase;
         }
+        if(popupState === "delivery")
+            return delivery;
+
         return emailPhase;
     }
 
     return (
-        <>{chooseContent()}</>
+        <>
+            {chooseContent()}
+            <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
+        </>
     )
 }
 export default EmailPopup;
--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -2,7 +2,8 @@ import { isProduction } from "../helpers/isProduction";
 
 export const getDomain = () => {
   const prodUrl = "https://sopra-fs22-group-23-server.herokuapp.com/";
-  const devUrl = "http://localhost:8080";
+  const devUrl = "https://sopra-fs22-group-23-server.herokuapp.com/";
+  // const devUrl = "http://localhost:8080";
 
   return isProduction() ? prodUrl : devUrl;
 };
